5ee01970f704d0f75f6f137b11f8568e5b611c26
Relrige <91017454+Relrige@users.noreply.github.com>
2023-03-04 10:45:20 +0200

    Add files via upload

---
+++ Нова папка (3) – копія/Console_Socket_Client/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Console_Socket_Client
+{
+    class Program
+    {
+        public static DateTime DateTime { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ip = host.AddressList.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
+            const int PORT = 1700;
+            IPEndPoint serverEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), PORT);
+
+            Console.WriteLine("Client configured");
+            try
+            {
+                client.Connect(serverEndpoint);
+                if (client.Connected)
+                {
+                    Console.WriteLine("Client connected to the server");
+
+                    byte[] intInBytes = BitConverter.GetBytes(5);
+                    byte[] charInBytes = BitConverter.GetBytes('c');
+                    byte[] base64InBytes = Convert.FromBase64String("base64 string");
+                    byte[] stringInBytes = Encoding.UTF8.GetBytes("string");
+
+                    string message = "";
+                    Console.WriteLine("Enter 1 if you want date or 2 - time");
+                    int numb = Convert.ToInt32(Console.ReadLine());
+                    if (numb == 1)
+                    {
+                        message = "date";
+                    }
+                    else if (numb == 2)
+                    {
+                        message = "time";
+                    }
+                    
+                    byte[] bytesToSend = Encoding.UTF8.GetBytes(message);
+
+                    int countOfSent = client.Send(bytesToSend);
+                    Console.WriteLine($"Client sent to the server {countOfSent} bytes");
+
+
+                    byte[] buffer = new byte[1000];
+                    int countOfReceived = client.Receive(buffer);
+                    string gotMessage = Encoding.UTF8.GetString(buffer, 0, countOfReceived);
+                    Console.WriteLine($"{gotMessage}");
+
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Connection lost: " + e.Message);
+            }
+            finally
+            {
+                client.Close();
+                Console.WriteLine("Client finished work");
+            }
+
+
+
+        }
+    }
+}
---
+++ Нова папка (3)/04_AsyncSocket_Client/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_AsyncSocket_Client
+{
+    class Program
+    {
+        static string postcode;
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            Console.WriteLine("enter postCode");
+            postcode = Console.ReadLine();
+            StartClient();
+            Console.WriteLine("Client finished work and closed");
+        }
+
+        private static void StartClient()
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                client.BeginConnect("192.168.31.148", 3101, ConnectCallback, client);
+                //Console.WriteLine("Client finished work and closed");
+
+                Console.ReadLine();
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Closing the client...");
+                client.Close();
+            }
+        }
+
+        private static void ConnectCallback(IAsyncResult ar)
+        {
+            var client = (Socket)ar.AsyncState;
+            //client.Connect("192.168.5.213", 3100);
+            client.EndConnect(ar);
+            Console.WriteLine("Connected to the server");
+
+            //Console.WriteLine("\nSending data ...");
+
+            byte[] bytes = Encoding.ASCII.GetBytes(postcode);
+            //client.Send(bytes);
+            
+            client.BeginSend(bytes, 0, bytes.Length, SocketFlags.None, SendCallback, client);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+
+            Console.WriteLine("Data sent.");
+            var client = (Socket)ar.AsyncState;
+            client.EndSend(ar);
+
+            Console.WriteLine("\nReceiving data ...");
+            const int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            Data data = new Data
+            {
+                Client = client,
+                Buffer = buffer
+            };
+            client.BeginReceive(buffer, 0, bufferSize, SocketFlags.None, ReceiveCallback, data);
+
+            ReceiveFromServer(client);
+        }
+
+        private static void ReceiveFromServer(Socket client)
+        {
+            Console.WriteLine("\nReceiving data ...");
+            const int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            Data data = new Data
+            {
+                Client = client,
+                Buffer = buffer
+            };
+            client.BeginReceive(buffer, 0, bufferSize, SocketFlags.None, ReceiveCallback, data);
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            Data data = (Data)ar.AsyncState;
+            Socket client = data.Client;
+            //int countOfBytes = client.Receive(buffer);
+            int countOfBytes = client.EndReceive(ar);
+
+            string receivedText = Encoding.UTF8.GetString(data.Buffer, 0, countOfBytes);
+            Console.WriteLine($"Answer from server ({countOfBytes} bytes):");
+            Console.WriteLine(receivedText);
+        }
+
+        public class Data
+        {
+            public Socket Client { get; set; }
+            public byte[] Buffer { get; set; }
+        }
+
+
+    }
+}
